fa89effbc2ed2e05ba74148856656fc557ed0e20
Thomas Siedler <ts@logentis.de>
2023-05-03 09:30:05 +0200

    shit gemacht

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import uuid
 
 from flask import Flask, request, jsonify, abort
+
 # initialize Flask server
 app = Flask(__name__)
 
@@ -25,6 +26,8 @@ todos = [
     {'id': todo_3_id, 'name': 'Kinokarten kaufen', 'description': '', 'list': todo_list_3_id},
     {'id': todo_3_id, 'name': 'Eier', 'description': '', 'list': todo_list_1_id},
 ]
+
+
 # add some headers to allow cross origin access to the API on this server, necessary for using preview in Swagger Editor!
 @app.after_request
 def apply_cors_header(response):
@@ -33,7 +36,8 @@ def apply_cors_header(response):
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
     return response
 
-@app.route('/todo-list',methods=['POST'])
+
+@app.route('/todo-list', methods=['POST'])
 def handleNewListEntrie():
     newList = request.get_json(force=True)
     if not newList:
@@ -43,6 +47,78 @@ def handleNewListEntrie():
             if value is "":
                 abort(400, "Die Liste benötigt ein Namen.")
     print("New Todo-List will be added: {}".format(newList))
+
+
+@app.route('/todo-list/<list_id>', methods=['GET', 'DELETE'])
+def handlelist(list_id):
+    list_by_id = None
+    for l in todo_lists:
+        if l['id'] == list_id:
+            list_by_id = l
+            break
+    if not (list_by_id):
+        abort(404)
+    if request.method == 'GET':
+        print("Found List with id: {} ".format(list_id))
+        return jsonify([i for i in todos if i['list'] == list_id])
+    if request.method == 'DELETE':
+        print("Deleting Todo-List with id : {}".format(list_id))
+        todo_lists.remove(list_by_id)
+        return '', 200
+
+
+@app.route('/todo-list/<list_id>/entry', methods=['POST'])
+def handleNewListItemEntrie(list_id):
+    newitem = request.get_json(force=True)
+    if not newitem:
+        abort(400, "Keine Daten in der Liste.")
+    if newitem['name'] == "":
+        abort(400, "Bitte Item-Name angeben.")
+    list_by_id = None
+    for l in todo_lists:
+        if l['id'] == list_id:
+            list_by_id = l
+            break
+    if not (list_by_id):
+        abort(404)
+    itemForList = {}
+    itemForList['id'] = uuid.uuid4()
+    for i in todos:
+        if i['id'] == itemForList['id']:
+            abort(404, "Fehler bei der erstellung der UUID.")
+    itemForList['name'] = newitem['name']
+    itemForList['description'] = newitem['description']
+    itemForList['list'] = list_by_id['id']
+    todos.append(itemForList)
+    print("New items will be added: {}".format(itemForList))
+    return itemForList, 200
+
+
+@app.route('/todo-list/<list_id>/entry/<entry_id>', methods=['PUT'])
+def editListItemEntry(list_id, entry_id):
+    newitem = request.get_json(force=True)
+    edited = None
+    if not newitem:
+        abort(400, "Keine Daten in der Liste.")
+    if newitem['name'] == "":
+        abort(400, "Bitte Item-Name angeben.")
+    for l in todos:
+        if str(l['id']) == entry_id and str(l['list']) == list_id:
+            edited = l
+            break
+    edited['name'] = newitem['name']
+    edited['description'] = newitem['description']
+    print("Item mit der Id: {} wurde editiert: {}".format(edited['id'], edited))
+    return edited, 200
+
+
+@app.route('/todo-list/<list_id>/entry/<entry_id>', methods=['DELETE'])
+def deleteListItemEntry(list_id, entry_id):
+    for i in todos:
+        if str(i['id']) == entry_id and str(i['list'] == list_id):
+            todos.remove(i)
+            return "Item erfolgreich gelöscht", 200
+    abort(404, "Fehlerhafte Ids")
 
 
 if __name__ == '__main__':
